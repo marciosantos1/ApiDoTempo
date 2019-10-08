b6acb1598a9986591e88e1005dd4ebd9bf8d3d5d
marcio <marcio_santos9@estudante.sc.senai.br>
2019-10-07 21:50:42 -0300

    #Criação da integração com API de previsão do tempo22.

--- index.php
+++ index.php
@@ -10,6 +10,8 @@
   
   $client = new Client(array('base_uri' => $baseUrl));
   
+  
+  
   $response = $client->get('/data/2.5/weather', array(
       'query' => array('appid' => $appid, 'id' => $id)
      
